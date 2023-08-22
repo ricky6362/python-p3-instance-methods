7a2f4a5ae39191fc8f9db43dc81bbc86528e3fd3
ricky6362 <63843083+ricky6362@users.noreply.github.com>
2023-08-22 21:18:11 +0300

    Assignment done

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,12 @@ class Person:
     # Class body goes here
 
     #Instance method definition
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
     pass
+
+guido = Person()
+print(guido.talk)
